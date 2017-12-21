6f36b844492e9fb000913b6d3f971bcd0a3cb94f
Rukaya Johaadien <JohaadienR@sanbi.local>
2017-12-21 11:20:14 +0200

    Adding the default settings file

---
+++ settings.default.py
@@ -0,0 +1,12 @@
+# These are local settings which are not committed to github
+
+seis_server = "172.1.1.1"
+seis_user = ""
+seis_password = ""
+seis_port = 1433
+seis_database = ''
+
+transcribe_server = "197.1.1.1"
+transcribe_user = ""
+transcribe_password = ""
+transcribe_database = ""
