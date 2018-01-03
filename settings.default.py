6b208b6e8befb970dde41f45d497fb6a9855cbb7
Rukaya Johaadien <JohaadienR@sanbi.local>
2018-01-03 12:35:04 +0200

    Adding more default settings

--- settings.default.py
+++ settings.default.py
@@ -9,4 +9,14 @@ seis_database = ''
 transcribe_server = "197.1.1.1"
 transcribe_user = ""
 transcribe_password = ""
+transcribe_port = 5432
 transcribe_database = ""
+
+max_width = 1600
+max_height = 2500
+
+seis_img_dir = 'C:\\Mydir'
+transcribe_img_dir = 'C:\\Mydir'
+seis_image_server_dir = 'http://196.21.170.68/webgate/resources/Digivol/' 
+
+transcribe_expedition_size = 500
